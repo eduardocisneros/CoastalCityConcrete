c1be3e765a598e1bc6e909aaea9d52011b335fd7
EduardoCisneros <eduardo.cisnerosm@hotmail.com>
2024-08-08 16:19:48 -0700

    working on hamburger menu

--- js/principalPage.js
+++ js/principalPage.js
@@ -1,1 +1,12 @@
-console.log("Hello world")
+const listMenu = document.getElementById("list_menu");
+const hamburgerMenu = document.getElementById("hamburger_menu");
+
+hamburgerMenu.addEventListener("click", ()=>{
+    listMenu.classList.toggle("show-me")
+    console.log(listMenu)
+});
+/* document.addEventListener('click', (e) => {
+    if (e.target !== hamburgerMenu && e.target !== listMenu) {
+        listMenu.classList.remove("show-me");
+    }
+}); */
